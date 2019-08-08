d2531d2eb3be1bf6e0fa149c2f140fffb2c3a762
Balázs Grill <balazs.grill@incquerylabs.com>
2019-08-08 08:51:19 +0200

    Update arduino-toolchain.cmake

--- arduino-toolchain.cmake
+++ arduino-toolchain.cmake
@@ -5,10 +5,3 @@ include_directories("$ENV{ARDUINOCORE}/cores/arduino")
 include_directories("$ENV{ARDUINOCORE}/variants/standard")
 set(CMAKE_CXX_FLAGS "-MMD -mmcu=atmega328p -Wall -DF_CPU=16000000L -Os")
 set(CMAKE_C_FLAGS "-MMD -mmcu=atmega328p -Wall -DF_CPU=16000000L -Os")
-add_library(arduino-core STATIC
-	"$ENV{ARDUINOCORE}/cores/arduino/main.cpp"
-	"$ENV{ARDUINOCORE}/cores/arduino/wiring.c"
-	"$ENV{ARDUINOCORE}/cores/arduino/wiring_digital.c"
-	"$ENV{ARDUINOCORE}/cores/arduino/wiring_analog.c"
-	"$ENV{ARDUINOCORE}/cores/arduino/hooks.c"
- )
