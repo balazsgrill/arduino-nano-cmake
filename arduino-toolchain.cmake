ef3984585dac3f7b49f7c010ca250a2aef0d2ce3
Balázs Grill <balazs.grill@incquerylabs.com>
2019-08-08 07:48:43 +0200

    Update arduino-toolchain.cmake

--- arduino-toolchain.cmake
+++ arduino-toolchain.cmake
@@ -1,14 +1,14 @@
 set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_C_COMPILER avr-gcc)
 set(CMAKE_CXX_COMPILER avr-g++)
-include_directories("${ARDUINOCORE}/cores/arduino")
-include_directories("${ARDUINOCORE}/variants/standard")
+include_directories("$ENV{ARDUINOCORE}/cores/arduino")
+include_directories("$ENV{ARDUINOCORE}/variants/standard")
 set(CMAKE_CXX_FLAGS "-MMD -mmcu=atmega328p -Wall -DF_CPU=16000000L -Os")
 set(CMAKE_C_FLAGS "-MMD -mmcu=atmega328p -Wall -DF_CPU=16000000L -Os")
 add_library(core STATIC
-	"${ARDUINOCORE}/cores/arduino/main.cpp"
-	"${ARDUINOCORE}/cores/arduino/wiring.c"
-	"${ARDUINOCORE}/cores/arduino/wiring_digital.c"
-	"${ARDUINOCORE}/cores/arduino/wiring_analog.c"
-	"${ARDUINOCORE}/cores/arduino/hooks.c"
+	"$ENV{ARDUINOCORE}/cores/arduino/main.cpp"
+	"$ENV{ARDUINOCORE}/cores/arduino/wiring.c"
+	"$ENV{ARDUINOCORE}/cores/arduino/wiring_digital.c"
+	"$ENV{ARDUINOCORE}/cores/arduino/wiring_analog.c"
+	"$ENV{ARDUINOCORE}/cores/arduino/hooks.c"
  )
